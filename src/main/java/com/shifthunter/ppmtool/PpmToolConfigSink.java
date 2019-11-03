5aeafbd0759e38e039e0119f5f26c768e158c52c
OsvaldoMartini <osvaldo.martini@gmail.com>
2019-11-03 12:38:18 +0100

    Renamimg package and class

--- src/main/java/com/shifthunter/ppmtool/PpmToolConfigSink.java
+++ src/main/java/com/shifthunter/ppmtool/PpmToolConfigSink.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.task.launcher.annotation.EnableTaskLauncher;
 
 @SpringBootApplication
 @EnableTaskLauncher
-public class PpmToolConfigSink {
+public class PPMToolConfigSink {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PpmToolConfigSink.class, args);
+		SpringApplication.run(PPMToolConfigSink.class, args);
 	}
 
 }
